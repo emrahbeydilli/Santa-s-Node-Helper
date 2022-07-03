41084d60b12f8c431960bd1b38504fea637defd4
emrahbeydilli <emrahbeydilli@gmail.com>
2022-07-03 23:40:22 +0300

    adding solution 2

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ const q1 = () =>{
 }
 
 q1();
+
+const q2 = () =>{
+    fs.readFile('./santa.txt',(err,data)=>{
+        console.time('santa-time');
+        const directions = data.toString();
+        const directionsArray = directions.split('');
+        let acc = 0;
+        let counter = 0;
+        const answer = directionsArray.some((currentItem)=>{
+            if (currentItem==='(') {
+                acc+=1;
+            } else {
+                acc-=1;
+            }
+            counter ++;
+            return acc<0;
+        })
+        console.timeEnd('santa-time');
+        console.log('basement enter:',counter);
+    })
+}
+
+q2();
